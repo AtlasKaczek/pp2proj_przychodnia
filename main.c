4faea1e896dd558df18c1ba8f9883d8343ce71bd
Stanisław Kryj <cookieblackxd@gmail.com>
2022-03-22 11:13:20 +0100

    Dodanie danych do lekarzy

--- main.c
+++ main.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Lekarz
 {
-    int id;
+    unsigned int id;
     char imie[20];
     char nazwisko[20];
+    char pesel[10];
+    char adres[40];
+    char email[40];
+    char tel[9];
+    unsigned int waga;
+    unsigned int wzrost;
+    char OddzialNFZ[60];
+
     struct Lekarz* nastepny;
     struct Lekarz* poprzedni;
 };
 
-void DodajLekarzaNaKoniec(struct Lekarz **glowny, int id, char imie[], char nazwisko[]) {
+void DodajLekarzaNaKoniec(struct Lekarz **glowny, unsigned int id, char imie[], char nazwisko[], char pesel[], char adres[], char email[], char tel[], unsigned int waga, unsigned int wzrost, char OddzialNFZ[]) {
     
     struct Lekarz *nowyLekarz = NULL;
     
@@ -19,6 +28,13 @@ void DodajLekarzaNaKoniec(struct Lekarz **glowny, int id, char imie[], char nazw
     nowyLekarz->id = id;
     strcpy(nowyLekarz->imie, imie);
     strcpy(nowyLekarz->nazwisko, nazwisko);
+    strcpy(nowyLekarz->pesel, pesel);
+    strcpy(nowyLekarz->adres, adres);
+    strcpy(nowyLekarz->email, email);
+    strcpy(nowyLekarz->tel, tel);
+    nowyLekarz->waga = waga;
+    nowyLekarz->wzrost = wzrost;
+    strcpy(nowyLekarz->OddzialNFZ, OddzialNFZ);
 
     struct Lekarz *temp = *glowny;
 
@@ -45,7 +61,7 @@ void wyswietlLekarzy(struct Lekarz *glowny) {
     else {
         struct Lekarz *ten = glowny;
         do {
-            printf("%d %s %s\n", ten->id, ten->imie, ten->nazwisko);
+            printf("%d %s %s %s %s %s %s %d %d %s\n", ten->id, ten->imie, ten->nazwisko, ten->pesel, ten->adres, ten->email, ten->tel, ten->waga, ten->wzrost, ten->OddzialNFZ);
             ten = ten->nastepny;
         } while (ten != NULL);
     }
@@ -61,13 +77,13 @@ int main() {
 
     wyswietlLekarzy(glowny);
 
-    DodajLekarzaNaKoniec(&glowny, 123, "Adam", "Monetka");
-    DodajLekarzaNaKoniec(&glowny, 231, "Radoslaw", "Koscielec");
-    DodajLekarzaNaKoniec(&glowny, 321, "Mariusz", "Pudzianowski");
+    DodajLekarzaNaKoniec(&glowny, 123, "Adam", "Monetka", "0123456789", "Kielce, ul.Jana Pawla II 14", "amonetka@przychodnia.lekarz.pl", "012345678", 78, 178, "NFZ nr 135");
+    DodajLekarzaNaKoniec(&glowny, 231, "Radoslaw", "Koscielec", "0123456789", "Kielce, ul.Jana Pawla II 14", "rkoscielec@przychodnia.lekarz.pl", "012345678", 78, 178, "NFZ nr 135");
+    DodajLekarzaNaKoniec(&glowny, 321, "Mariusz", "Pudzianowski", "0123456789", "Kielce, ul.Jana Pawla II 14", "mpudzian@przychodnia.lekarz.pl", "012345678", 78, 178, "NFZ nr 135");
 
     wyswietlLekarzy(glowny);
 
-    DodajLekarzaNaKoniec(&glowny, 213, "Wikary", "Marcepan");
+    DodajLekarzaNaKoniec(&glowny, 213, "Wikary", "Marcepan", "0123456789", "Kielce, ul.Jana Pawla II 14", "wmaecepan@przychodnia.lekarz.pl", "012345678", 78, 178, "NFZ nr 135");
 
     wyswietlLekarzy(glowny);
     
